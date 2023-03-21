ddd7f75e80479077e047886cd9c01df18563577b
Erica <ericaemorabito@gmail.com>
2023-03-21 15:07:24 -0700

    starting info card

---
+++ restaurant_website/src/components/InfoCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/infoCard.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import bruschetta from "../images/bruschetta.jpeg";
+
+const InfoCard = () => {
+  return (
+    <>
+      <Container fluid>
+        <Row className="h-75 py-5 mx-5">
+          <Col sm={12} m={6} lg={6} className="py-3 d-flex flex-column align-items-center justify-content-center">
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <img
+                src={bruschetta}
+                className="info-card-image"
+                alt="tomato bruschetta on table"
+              ></img>
+            </div>
+          </Col>
+          <Col sm={12} m={6} lg={6} className="py-3 d-flex flex-column align-items-center justify-content-center">
+            <div className="info-card py-5 px-5">
+              <h2>Host a Party</h2>
+              <p>
+                We recommend celebrating all moments, big or small, or turning a
+                business dinner into a festive feast. At Colletta, you don't
+                need an excuse to gather.
+              </p>
+              <Button variant="dark">Make Reservation</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default InfoCard;
--- restaurant_website/src/screens/HomeScreen.js
+++ restaurant_website/src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Blurb from '../components/Blurb'
+import InfoCard from '../components/InfoCard'
 const HomeScreen = () => {
   return (
     <div>
       <h1>Colletta</h1>
       <Blurb></Blurb>
+      <InfoCard/>
 
     </div>
   )
