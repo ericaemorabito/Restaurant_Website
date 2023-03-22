e20e7972402e84f1d2ea05da15a17d146f43acc2
Erica <ericaemorabito@gmail.com>
2023-03-22 16:41:09 -0700

    styled header

--- restaurant_website/src/components/Header.js
+++ restaurant_website/src/components/Header.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "bootstrap";
-import brand from "../images/brand.png";
+import Colleta from "../images/Colleta.png";
 import { Nav, Navbar, Container, Button } from "react-bootstrap";
 import '../styles/header.css';
 
 const Header = (props) => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
-        <Container>
+      <Navbar expand="lg">
           <Navbar.Brand href="/">
             <img
-              src={brand}
-              width="50"
-              height="50"
+              src={Colleta}
+              width="200"
+              height="70"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
             ></img>
@@ -21,13 +20,12 @@ const Header = (props) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav id='nav' as='div' className="justify-contents-end">
-              <Button className="nav-links text-dark" onClick={props.handleClose}>Hours & Location</Button>
-              <Nav.Link className="nav-links text-dark" href="/menu">Menu</Nav.Link>
-              <Nav.Link className="nav-links text-dark" href="/about">About</Nav.Link>
-              <Button className="nav-links text-dark" onClick={props.handleClose}>Reservations</Button>
+              <Button className="nav-links nav-buttons" onClick={props.handleClose}>Hours & Location</Button>
+              <Nav.Link className="nav-links" href="/menu">Menu</Nav.Link>
+              <Nav.Link className="nav-links" href="/about">About</Nav.Link>
+              <Button className="nav-links nav-buttons" onClick={props.handleClose}>Reservations</Button>
             </Nav>
           </Navbar.Collapse>
-        </Container>
       </Navbar>
     </header>
   );
