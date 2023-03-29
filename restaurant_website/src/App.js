d4b0ed4a439189d97dcd2cefce3944b8cd960a56
Erica <ericaemorabito@gmail.com>
2023-03-28 17:10:51 -0700

    prepare for deployment

--- restaurant_website/src/App.js
+++ restaurant_website/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "react-bootstrap/dist/react-bootstrap.min.js";
 import HomeScreen from "./screens/HomeScreen";
