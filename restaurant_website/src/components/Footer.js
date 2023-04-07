7fc4150baec367aa6b51e77db65ad404df803a39
Erica <ericaemorabito@gmail.com>
2023-04-07 12:47:58 -0700

    prepare to deploy

--- restaurant_website/src/components/Footer.js
+++ restaurant_website/src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/footer.css'
 const Footer = ({handleHours, makeReservation}) => {
   return (
     <footer className="mb-3 mt-2 d-flex justify-content-center align-items-center">
-          <p>CONTACT</p>
+          {/* <p>CONTACT</p> */}
           <button className="footer-btn" onClick={handleHours}>HOURS & LOCATION</button>
           <button className="footer-btn" onClick={makeReservation}>RESERVATIONS</button>
     </footer>
